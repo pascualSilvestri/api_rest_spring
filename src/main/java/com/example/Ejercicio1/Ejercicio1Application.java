ec4cc771ecbe4e450b71d8f794cb2b1bf946d67f
pascual_silvestri <pascualsilvestri14@gmail.com>
2023-04-14 22:01:55 -0300

    modi

--- src/main/java/com/example/Ejercicio1/Ejercicio1Application.java
+++ src/main/java/com/example/Ejercicio1/Ejercicio1Application.java
@@ -12,25 +12,7 @@ public class Ejercicio1Application {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(Ejercicio1Application.class, args);
 
-		LaptoRepository repository = context.getBean(LaptoRepository.class);
-
-		Lapto lapto1 = new Lapto(
-				null,
-				"Hp",
-				"HP-bsa14542",
-				150000.0);
-
-		Lapto lapto2 = new Lapto(
-				null,
-				"ASUS",
-				"RAPTOR",
-				450000.0);
-
-		repository.save(lapto1);
-		repository.save(lapto2);
-
-		System.out.println(repository.findAll().size());
-
+		
 	}
 
 
